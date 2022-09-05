bd4c7888c417448ba0666d3c3d887c6ee4637da9
Michelle David <rachaelholy@gmail.com>
2022-09-06 00:10:12 +0100

    Created Quiz component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import HomePage from "./components/HomePage.js"
+import HomePage from "./components/HomePage.js";
+import {useState, useEffect} from 'react';
+import QuizPage from "./components/QuizPage.js";
 
 export default function App(){
+    const [allQuiz, setAllQuiz] = useState([])
+
+    const mapArray = allQuiz.map(quiz => {
+        return <QuizPage />
+    })
     return(
-        <HomePage  />
+        <main>
+            <HomePage  />
+        <QuizPage />
+        </main>
     )
 }
 
-/*"react/cjs/react.production.min"
-
- const mapArray = allQuiz.map(quiz => <HomePage startQuiz={startQuiz}/>)
-
-    useEffect(() => {
-        async function startQuiz() {
-            const res = await fetch("https://opentdb.com/api.php?amount=10&category=9&type=multiple")
-            const data = await res.json()
-            setAllQuiz(data.data.memes)
-            
-        }
-        startQuiz()
-        
-        return () => {
-            
-        }
-    }, []) */
---
+++ src/components/QuizPage.js
@@ -0,0 +1,15 @@
+import '../QuizPage.css';
+
+export default function QuizPage(){
+    return (
+        <>
+        <main className='quiz-container'>
+            <h2>?</h2>
+            <button className='btn1'>1</button>
+            <button className='btn2'>2</button>
+            <button className='btn3'>3</button>
+            <button className='btn4'>4</button>
+        </main>
+        </>
+    )
+}
