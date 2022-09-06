6e9722201932a1b5308ceac58a313f28d01e2239
Michelle David <rachaelholy@gmail.com>
2022-09-06 02:24:37 +0100

    Added some more styling

--- src/App.js
+++ src/App.js
@@ -16,3 +16,20 @@ export default function App(){
     )
 }
 
+/*"react/cjs/react.production.min"
+
+ const mapArray = allQuiz.map(quiz => <HomePage startQuiz={startQuiz}/>)
+
+    useEffect(() => {
+        async function startQuiz() {
+            const res = await fetch("https://opentdb.com/api.php?amount=10&category=9&type=multiple")
+            const data = await res.json()
+            setAllQuiz(data.data.memes)
+            
+        }
+        startQuiz()
+        
+        return () => {
+            
+        }
+    }, []) */
--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -4,11 +4,16 @@ export default function QuizPage(){
     return (
         <>
         <main className='quiz-container'>
-            <h2>?</h2>
-            <button className='btn1'>1</button>
-            <button className='btn2'>2</button>
-            <button className='btn3'>3</button>
-            <button className='btn4'>4</button>
+            <img className='quiz-img' src='images/blob.png' alt='img'/>
+          <div className="quiz-info">
+          <h2 className="question">Here's my question?</h2><br/><br />
+            <button className='btn1 btn'>1 in the</button>
+            <button className='btn2 btn'>2</button>
+            <button className='btn3 btn'>3</button>
+            <button className='btn4 btn'>4</button>
+            <hr />
+          </div>
+            <button className='result-btn'>Check answer</button>
         </main>
         </>
     )
