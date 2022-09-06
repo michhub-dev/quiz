f59307b7a041f864b721b3cd6f201af4d088e048
Michelle David <rachaelholy@gmail.com>
2022-09-06 14:44:30 +0100

    Styling modification

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,7 @@ export default function HomePage(){
         <>
         <main className="home-container">
                 <img src="images/blob.png" alt="img" className="background-img" />
-                <img src="" alt="" className="" />
+              
                 <div className="description">
                         <h1 className="title">Quizzical</h1>
                         <p className="home-text">Can you answer these questions correctly? Let's find out</p>
